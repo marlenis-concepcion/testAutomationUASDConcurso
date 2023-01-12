d265f81c821065d443758c6d99f560416e919085
marlenis-concepcion <marlenis.concepci@gmail.com>
2023-01-11 23:32:49 -0400

    prueba a amiga

--- cypress/e2e/Demospec.cy.js
+++ cypress/e2e/Demospec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import { tutorialpages } from "../support/guru99pages/tutorialpages";
+ let tp = new tutorialpages();
+
+
+describe('Insurance Project', () => {
+  it('Insurance Project filling form', () => {
+    cy.visit('https://demo.guru99.com/insurance/v1/index.php')
+    cy.xpath('//a[@href="register.php"][contains(.,"Register")]').click({ force: true });
+    cy.xpath('//input[@id="user_firstname"]').type("Riserani SinSegundo");
+
+
+
   })
+
 })
--- cypress/support/guru99pages/tutorialpages.js
+++ cypress/support/guru99pages/tutorialpages.js
@@ -1,17 +1,24 @@
 'use strict';
 export class tutorialpages {
 
+    //form Insurance Project
+
+    
 verifypagesLinkisDisplayed(){
     cy.wait(4000);
     // First tab
         cy.xpath('//a[@href="#"][contains(.,"Selenium")]').click({ force: true })
         cy.xpath('//a[contains(.,"Yahoo")]').should('contain', "Yahoo");
+        cy.wait(4000);
+        cy.xpath('(//b[@class="caret"])[1]').click({ force: true })
+        cy.reload();
 
-    //Second tab
-    cy.wait(40000);
-    cy.xpath('//a[contains(.,"Insurance Project")]').click({ force: true })
+    // //Second tab
+    // cy.wait(40000);
+    // cy.visit("https://demo.guru99.com/insurance/v1/index.php");
+    // //cy.xpath('//a[contains(.,"Insurance Project")]').dblclick({ force: true })
 
-      //  cy.get('.popover-header').should('contain', matchDetailClTitle)
+    //   //  cy.get('.popover-header').should('contain', matchDetailClTitle)
 
     return this;
 }
