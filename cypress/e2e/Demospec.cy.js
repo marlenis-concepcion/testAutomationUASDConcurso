020b3ae47596c27b45a502b2478c92c69d48fabe
marlenis-concepcion <marlenis.concepci@gmail.com>
2023-01-11 23:33:04 -0400

    ...

--- cypress/e2e/Demospec.cy.js
+++ cypress/e2e/Demospec.cy.js
@@ -6,7 +6,7 @@ describe('Insurance Project', () => {
   it('Insurance Project filling form', () => {
     cy.visit('https://demo.guru99.com/insurance/v1/index.php')
     cy.xpath('//a[@href="register.php"][contains(.,"Register")]').click({ force: true });
-    cy.xpath('//input[@id="user_firstname"]').type("Riserani SinSegundo");
+    cy.xpath('//input[@id="user_firstname"]').type("SinNombre SinSegundo");
 
 
 
